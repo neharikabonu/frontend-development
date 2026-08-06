e14a88aded3857f7bdd09d8692fb4cba84e8efe2
Neharika <neharikabonu@gmail.com>
2026-08-06 11:43:04 +0530

    Learned conditional statements

---
+++ JavaScript/Conditional/nested-if.js
@@ -0,0 +1,12 @@
+let age = 20;
+let hasID = true;
+
+if (age >= 18) {
+    if (hasID) {
+        console.log("Entry Allowed");
+    } else {
+        console.log("Bring your ID");
+    }
+} else {
+    console.log("Not Eligible");
+}
