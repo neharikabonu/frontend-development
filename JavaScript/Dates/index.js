0fa17618040fede6bb4a16485f620558c26a09ba
Neharika <neharikabonu@gmail.com>
2026-08-07 10:10:51 +0530

    Learned Date()

---
+++ JavaScript/Dates/index.js
@@ -0,0 +1,27 @@
+let date = new Date()
+
+console.log(date.toString())
+
+console.log(date.getDate())
+
+console.log(date.getFullYear())
+
+console.log(date.getMonth())
+
+console.log(date.getTime());
+
+console.log(Date.now());
+
+date.setFullYear(2030);
+
+console.log(date);
+
+console.log(date.toDateString());
+
+console.log(date.toTimeString());
+
+console.log(date.toLocaleDateString());
+
+console.log(date.toLocaleString());
+
+console.log(date.toISOString());
