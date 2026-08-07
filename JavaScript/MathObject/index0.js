97fb4a059870676cac02610aee8c207347283965
Neharika <neharikabonu@gmail.com>
2026-08-07 09:07:10 +0530

    Learned MathObject and how to generate OTP

---
+++ JavaScript/MathObject/index0.js
@@ -0,0 +1,49 @@
+console.log(Math.PI);
+
+console.log(Math.sqrt(25));
+
+console.log(Math.max(10, 20, 30));
+
+console.log(Math.min(5,2,6,7,1,0))
+
+console.log(Math.round(4.4)); // 4 < 4.5
+
+console.log(Math.floor(4.9));
+
+console.log(Math.floor(-4.1)); //-5 moves towards -infinity
+
+console.log(Math.ceil(4.1));
+
+console.log(Math.ceil(-4.9)); //-4 
+
+console.log(Math.trunc(4.9)); //removes the decimal part
+
+console.log(Math.pow(2,3));
+
+console.log(Math.sign(20));
+// 1 -> positive
+// -1 -> negative
+// 0
+// -0
+// NaN
+
+console.log(Math.random())
+
+//0 to 9
+
+console.log(Math.floor(Math.random() * 10))
+
+//1 to 10
+
+console.log(Math.floor(Math.random() * 10) + 1)
+
+//Math.floor(Math.random() * (max - min + 1)) + min
+
+// 0 to 10
+
+console.log(Math.floor(Math.random() * (10 - 0 + 1)))
+
+//4 digit OTP 1000 - 9999
+
+console.log(Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000)
+
