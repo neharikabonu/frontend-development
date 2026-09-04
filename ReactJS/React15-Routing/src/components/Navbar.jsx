07047ac23752db853952cdd4a5ac299bd1259e88
Neharika <neharikabonu@gmail.com>
2026-09-04 09:54:44 +0530

    Learned basic routing

---
+++ ReactJS/React15-Routing/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h1>Logo</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ ReactJS/React15-Routing/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='page'>
+      About
+    </div>
+  )
+}
+
+export default About
